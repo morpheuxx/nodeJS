dccec3a74bb99efc97d5ccbe74459f88e73747ea
morpheuxx <59151357+morpheuxx@users.noreply.github.com>
2021-06-13 15:08:57 -0300

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,43 @@
+
+
+//ainda iremos implementar uma logica melhor que essa abaixo,
+// mas caso queira testar só tirar do comentário
+
+
+
+// var count=0
+// var count2=0
+// var tvend=13
+
+// document.querySelectorAll(".mais").forEach(function(btn) {
+//     btn.addEventListener("click", function(){
+//       count=count+1;
+//       var vtotal = document.getElementById("totUnidade");
+//       var vtotal2 = document.getElementById("totalProduto");
+//       var vtotal3 = document.getElementById("ValorFinal");
+//       count2=count2 + tvend;
+//       vtotal.innerHTML='R$ '+count2+ ',00';
+//       vtotal2.innerHTML='R$ '+count2+ ',00';
+//       vtotal3.innerHTML='R$ '+count2+ ',00';
+//       campo.setAttribute('value', count);
+//     });
+//   });
+  
+//   document.querySelectorAll(".menos").forEach(function(btn) {
+//     btn.addEventListener("click", function(){
+//         if(count >= 1){
+//           count=count-1;
+//             var campo = document.getElementById("campo");
+//             var vtotal = document.getElementById("totUnidade");
+//             var vtotal2 = document.getElementById("totalProduto");
+//             var vtotal3 = document.getElementById("ValorFinal");
+//             count2=count2 - tvend;
+//             vtotal.innerHTML='R$ '+count2+ ',00';
+//             vtotal2.innerHTML='R$ '+count2+ ',00';
+//             vtotal3.innerHTML='R$ '+count2+ ',00';
+//             campo.setAttribute('value', count);
+//         }else{
+//             alert('Você removeu todos os produtos');
+//           }
+//         });
+//   });
---
+++ templates/form.marko.js
@@ -0,0 +1,38 @@
+// Compiled using marko@4.23.9 - DO NOT EDIT
+"use strict";
+
+var marko_template = module.exports = require("marko/src/html").t(__filename),
+    marko_componentType = "/templateAllColl-main$1.0.0/templates/form.marko",
+    marko_renderer = require("marko/src/runtime/components/renderer"),
+    marko_loadTag = require("marko/src/runtime/helpers/load-tag"),
+    init_components_tag = marko_loadTag(require("marko/src/core-tags/components/init-components-tag")),
+    await_reorderer_tag = marko_loadTag(require("marko/src/core-tags/core/await/reorderer-renderer")),
+    _preferred_script_location_tag = marko_loadTag(require("marko/src/core-tags/components/preferred-script-location-tag"));
+
+function render(input, out, __component, component, state) {
+  var data = input;
+
+  out.w("<!DOCTYPE html><html><head><meta charset=utf-8><meta name=viewport content=\"width=device-width, initial-scale=1.0, shrink-to-fit=no\"><title>Cadastro</title><link rel=stylesheet href=/assets/css/style-extra.css><link rel=stylesheet href=/assets/bootstrap/css/bootstrap.min.css?h=a847d1fe541ca62044cce96cfd476e07><link rel=stylesheet href=https://fonts.googleapis.com/css?family=Lato><link rel=stylesheet href=https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900><link rel=stylesheet href=https://fonts.googleapis.com/css?family=Muli><link rel=stylesheet href=https://fonts.googleapis.com/css?family=Aldrich><link rel=stylesheet href=https://fonts.googleapis.com/css?family=Alfa+Slab+One><link rel=stylesheet href=https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css><link rel=stylesheet href=https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css><link rel=stylesheet href=/assets/css/styles.min.css?h=de827f96ca45107a1c2d70d42c066210></head><body id=page-top style=\"height: 645px;\"><nav class=\"navbar navbar-expand-lg navbar-light  fixed-top\" style=\"background-color: #FDC844;\"><a class=navbar-brand href=/ ><img id=logo src=../assests/img/logo.png alt=logo></a><button class=navbar-toggler type=button data-toggle=collapse data-target=#conteudoNavbarSuportado aria-controls=conteudoNavbarSuportado aria-expanded=false aria-label=\"Alterna navegação\"><span class=navbar-toggler-icon></span></button><div class=\"collapse navbar-collapse\" id=conteudoNavbarSuportado><ul class=\"navbar-nav mr-auto\"><li class=nav-item><a class=\"nav-link menu-letter\" href=/ >Catálogo <span class=sr-only>(página atual)</span></a></li><li class=\"nav-item menu-letter\"><a class=nav-link href=/produtos>Produtos</a></li><li class=\"nav-item menu-letter\"><a class=nav-link href=/pagamentos>Carrinho</a></li></ul><form class=\"form-inline my-2 my-lg-0\"><a class=\"btn btn-outline-dark menu-letter\" href=/cadastro role=button id=hover style=\"background-color: #272319; border-radius: 5px; border: none; color: #ffffff; margin-right: 20px;\">CADASTRO</a><a class=\"btn btn-outline-dark menu-letter\" href=# role=button id=hover2 style=\"background-color: #ffffff; border-radius: 5px; border: none; color: #161313;\">LOGIN</a></form></div></nav><section class=bg-white><div class=container><form action=/registro method=POST><div class=form-group><br><label for=nome_completo>Nome Completo</label><input type=text class=form-control name=nome id=nome_completo placeholder=\"Nome Completo\"></div><div class=\"form-group row\"><div class=\"form-group col-md-6\"><label for=date_nascimento>Data de Nascimento</label><input class=form-control type=date name=date value=2011-08-10 id=data_nascimento></div><div class=\"form-group col-md-6\"><label for=cpf>CPF</label><input type=number class=form-control name=cpf id=cpf placeholder=88888888888></div></div><div class=form-group><label for=endereco>Endereço</label><input type=text class=form-control id=endereco name=endereco placeholder=\"Av. Barão de Lucena, 1111\"></div><div class=form-row><div class=\"form-group col-md-6\"><label for=cidade>Cidade</label><input type=text class=form-control id=cidade name=cidade placeholder=\"Jaboatão dos Guararapes\"></div><div class=\"form-group col-md-4\"><label for=estado>Estado</label><input type=text class=form-control id=estado name=estado placeholder=Pernambuco></div><div class=\"form-group col-md-2\"><label for=cep>CEP</label><input type=text class=form-control id=cep name=cep placeholder=540900-000></div></div><div class=form-row><div class=\"form-group col-md-6\"><label for=inputEmail4>Email</label><input type=email class=form-control id=inputEmail4 name=email placeholder=fulano@gmail.com></div><div class=\"form-group col-md-6\"><label for=inputPassword4>Senha</label><input type=password class=form-control id=senha name=senha placeholder=Senha></div></div><button type=reset class=\"btn btn-danger\">Cancelar</button><button type=submit class=\"btn btn-primary\">Salvar</button></form></div></section><div class=footer-clean style=\"width: 100%;height: 146px;padding: 0;\"><footer style=\"background: rgb(255, 192, 34);color: rgb(0,0,0);\"><div class=container><div class=\"row justify-content-center\"><div class=\"col-sm-4 col-md-3 item\"><h3>Serviços</h3><ul><li><a href=#>Web design</a></li><li><a href=#>Desenvolvimento</a></li></ul></div><div class=\"col-sm-4 col-md-3 item\"><h3>Sobre</h3><ul><li><a href=#>Compania</a></li><li><a href=#>Time</a></li></ul></div><div class=\"col-lg-3 item social\"><a href=# class=socialm><i class=\"icon ion-email\"></i></a><a href=# class=socialm><i class=\"icon ion-social-facebook\"></i></a><a href=# class=socialm><i class=\"icon ion-social-whatsapp\"></i></a><a href=# class=socialm><i class=\"icon ion-social-instagram\"></i></a><p class=copyright>All Cool Distribuidora © 2020</p></div></div></div></footer></div><script src=https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js></script><script src=https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.bundle.min.js></script><script src=https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js></script><script src=/assets/js/script.min.js?h=7a4e021987da3d38b958d06f38b30142></script>");
+
+  init_components_tag({}, out);
+
+  await_reorderer_tag({}, out, __component, "97");
+
+  _preferred_script_location_tag({}, out);
+
+  out.w("</body></html>");
+}
+
+marko_template._ = marko_renderer(render, {
+    ___implicit: true,
+    ___type: marko_componentType
+  });
+
+marko_template.meta = {
+    id: "/templateAllColl-main$1.0.0/templates/form.marko",
+    tags: [
+      "marko/src/core-tags/components/init-components-tag",
+      "marko/src/core-tags/core/await/reorderer-renderer",
+      "marko/src/core-tags/components/preferred-script-location-tag"
+    ]
+  };
